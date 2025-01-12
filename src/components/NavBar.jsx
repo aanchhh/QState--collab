5092aa6fc84c991c3784f503610760eb8b20e4e4
Riya Mehta <riyamehta1608@gmail.com>
2025-01-12 15:04:13 +0530

    Changes in Hamburger Menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -68,9 +68,12 @@ const NavBar = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden lg:flex"
-          } fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto  lg:bg-opacity-50`}
         >
-          <div className="flex flex-col items-center justify-center lg:flex-row cursor-pointer backdrop-blur-lg pl-4 pr-4 bg-n-3 bg-opacity-20 border border-n-3 rounded-3xl -translate-x-6">
+          <div
+            className="flex flex-col items-center translate-y-52 w-full 
+           justify-center lg:flex-row cursor-pointer backdrop-blur-lg pl-4 pr-4  bg-opacity-20 border border-n-3 rounded-3xl -translate-x-6"
+          >
             {navigation.map((item) => (
               <a
                 key={item.id}
@@ -90,42 +93,43 @@ const NavBar = () => {
 
           <HamburgerMenu />
         </nav>
-        <div className="backdrop-blur-xl border border-n-2 flex text-center bg-n-1 bg-opacity-5 translate-y-1 -translate-x-16 rounded-2xl">
-          {!auth.isAuthenticated && (
-            <a
-              onClick={() => navigate("/sign_up")}
-              className="hidden mr-8 text-white translate-y-3 translate-x-4 font-code font-medium transition-colors rounded-xl hover:text-n-3 hover:cursor-pointer lg:block"
-            >
-              REGISTER
-            </a>
-          )}
-          {!auth.isAuthenticated && (
+        <div className="absolute right-8">
+          <div className="backdrop-blur-xl border border-n-2 flex text-center bg-n-1 bg-opacity-5 translate-y-1 -translate-x-16 rounded-2xl">
+            {!auth.isAuthenticated && (
+              <a
+                onClick={() => navigate("/sign_up")}
+                className="hidden mr-8 text-white translate-y-3 translate-x-4 font-code font-medium transition-colors rounded-xl hover:text-n-3 hover:cursor-pointer lg:block"
+              >
+                REGISTER
+              </a>
+            )}
+            {!auth.isAuthenticated && (
+              <Button
+                className="hidden lg:flex bg-gradient-to-r from-n-3 to-n-4 rounded-2xl border border-l-2 border-l-n-2"
+                onClick={() => navigate("/sign_in")}
+              >
+                Log in
+              </Button>
+            )}
+            {auth.isAuthenticated && (
+              <a
+                onClick={() => {
+                  dispatch(logout());
+                  navigate("/");
+                }}
+                className="hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+              >
+                Logout
+              </a>
+            )}
             <Button
-              className="hidden lg:flex bg-gradient-to-r from-n-3 to-n-4 rounded-2xl border border-l-2 border-l-n-2"
-              onClick={() => navigate("/sign_in")}
+              className="ml-auto lg:hidden"
+              px="px-3"
+              onClick={toggleNavigation}
             >
-              Log in
+              <MenuSvg openNavigation={openNavigation} />
             </Button>
-          )}
-          {auth.isAuthenticated && (
-            <a
-              onClick={() => {
-                dispatch(logout());
-                navigate("/");
-              }}
-              className="hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-            >
-              Logout
-            </a>
-          )}
-
-          <Button
-            className="ml-auto lg:hidden"
-            px="px-3"
-            onClick={toggleNavigation}
-          >
-            <MenuSvg openNavigation={openNavigation} />
-          </Button>
+          </div>
         </div>
       </div>
     </div>
