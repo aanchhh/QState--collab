fe6ef0e8983efa5d7dfb8ffc918ea61dbbe94277
Riya Mehta <riyamehta1608@gmail.com>
2025-01-12 19:03:43 +0530

    Fixed Hamburger Menu and Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           }}
           className="relative flex flex-col gap-4 items-center justify-center px-4"
         >
-          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center ">
             <div>
               <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6  bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
                 <Cover>Streamline</Cover> your Business Processes Using
@@ -36,8 +36,8 @@ const Hero = () => {
               </h1>
             </div>
           </div>
-          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-            <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+          <div className="font-extralight  dark:text-neutral-200 py-4">
+            <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 text-center md:text-base lg:text-lg lg:mb-8">
               Unleash the Maximum Efficiency. Upgrade your productivity with
               Qstate&apos;s custom AI Agents.
             </p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,139 +1,157 @@
-import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { qstate_logo } from "../assets";
-import Button from "./Button";
-import MenuSvg from "../assets/svg/MenuSvg";
-import { HamburgerMenu } from "./design/Header";
-import { logout } from "../authSlice";
+// import React, { useState, useEffect } from "react";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { qstate_logo } from "../assets";
+// import Button from "./Button";
+// import MenuSvg from "../assets/svg/MenuSvg";
+// import { HamburgerMenu } from "./design/Header";
+// import { logout } from "../authSlice";
+
+// const NavBar = () => {
+//   const navigate = useNavigate();
+//   const pathname = useLocation();
+//   const [openNavigation, setOpenNavigation] = useState(false);
+//   const [scrollingUp, setScrollingUp] = useState(true); // Initially true to show navbar on load
+//   const auth = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+//   const [lastScrollY, setLastScrollY] = useState(0); // To track scroll position
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const currentScrollY = window.scrollY;
+//       if (currentScrollY > lastScrollY) {
+//         // Scrolling down
+//         setScrollingUp(false);
+//       } else {
+//         // Scrolling up
+//         setScrollingUp(true);
+//       }
+//       setLastScrollY(currentScrollY);
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, [lastScrollY]);
+
+//   const toggleNavigation = () => {
+//     setOpenNavigation(!openNavigation);
+//   };
+
+//   const navigation = [
+//     { id: 1, title: "Home", url: "/" },
+//     { id: 2, title: "Services", url: "/services" },
+//     { id: 3, title: "Pricing", url: "/pricing" },
+//     { id: 4, title: "Blogs", url: "/blogs" },
+//     { id: 5, title: "Contact", url: "/contact" },
+//   ];
+
+//   return (
+//     <div
+//       className={`fixed ${
+//         scrollingUp ? "-top-10" : "-top-[200px]" // Navbar comes back when scrolling up, moves up when scrolling down
+//       } left-[32px] w-full p-2 h-20 justify-center z-50 text-center pl-4 pr-4 transition-all duration-300 ease-in-out`}
+//     >
+//       <div
+//         className="flex items-center px-5 lg:px-10 py-5
+//       translate-y-10"
+//       >
+//         <a
+//           className="block w-[12rem] xl:mr-8 cursor-pointer"
+//           onClick={() => navigate("/")}
+//         >
+//           <img src={qstate_logo} width={190} height={40} alt="Q State" />
+//         </a>
+
+//         <nav
+//           className={`${
+//             openNavigation ? "flex" : "hidden lg:flex"
+//           } fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto  lg:bg-opacity-50`}
+//         >
+//           <div
+//             className="flex flex-col items-center w-full
+//            justify-center lg:flex-row cursor-pointer backdrop-blur-lg pl-4 pr-4  bg-opacity-20 border border-n-3 rounded-3xl -translate-x-6"
+//           >
+//             {navigation.map((item) => (
+//               <a
+//                 key={item.id}
+//                 onClick={() => {
+//                   navigate(item.url);
+//                 }}
+//                 className={`block font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+//                   item.url === pathname.pathname
+//                     ? "lg:text-n-1"
+//                     : "lg:text-n-1/50"
+//                 } px-6 py-6 lg:py-4 lg:text-sm lg:font-semibold`}
+//               >
+//                 {item.title}
+//               </a>
+//             ))}
+//           </div>
+
+//           <HamburgerMenu />
+//         </nav>
+//         <div className="right-8">
+//           <div className="backdrop-blur-xl border border-n-2 flex text-center bg-n-1 bg-opacity-5 translate-y-1 -translate-x-16 rounded-2xl">
+
+//             {!auth.isAuthenticated && (
+//               <a
+//                 onClick={() => navigate("/sign_up")}
+//                 className="hidden mr-8 text-white translate-y-3 translate-x-4 font-code font-medium transition-colors rounded-xl hover:text-n-3 hover:cursor-pointer lg:block"
+//               >
+//                 REGISTER
+//               </a>
+//             )}
+//             {!auth.isAuthenticated && (
+//               <Button
+//                 className="hidden lg:flex bg-gradient-to-r from-n-3 to-n-4 rounded-2xl border border-l-2 border-l-n-2"
+//                 onClick={() => navigate("/sign_in")}
+//               >
+//                 Log in
+//               </Button>
+//             )}
+//             {auth.isAuthenticated && (
+//               <a
+//                 onClick={() => {
+//                   dispatch(logout());
+//                   navigate("/");
+//                 }}
+//                 className="hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+//               >
+//                 Logout
+//               </a>
+//             )}
+//             <Button
+//               className="ml-auto lg:hidden"
+//               px="px-3"
+//               onClick={toggleNavigation}
+//             >
+//               <MenuSvg openNavigation={openNavigation} />
+//             </Button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
+import React from "react";
+import NavBarDesktop from "./NavBarDesktop";
+import NavBarMobile from "./NavBarMobile";
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const pathname = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(false);
-  const [scrollingUp, setScrollingUp] = useState(true); // Initially true to show navbar on load
-  const auth = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const [lastScrollY, setLastScrollY] = useState(0); // To track scroll position
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY) {
-        // Scrolling down
-        setScrollingUp(false);
-      } else {
-        // Scrolling up
-        setScrollingUp(true);
-      }
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
-
-  const toggleNavigation = () => {
-    setOpenNavigation(!openNavigation);
-  };
-
-  const navigation = [
-    { id: 1, title: "Home", url: "/" },
-    { id: 2, title: "Services", url: "/services" },
-    { id: 3, title: "Pricing", url: "/pricing" },
-    { id: 4, title: "Blogs", url: "/blogs" },
-    { id: 5, title: "Contact", url: "/contact" },
-  ];
-
   return (
-    <div
-      className={`fixed ${
-        scrollingUp ? "-top-10" : "-top-[200px]" // Navbar comes back when scrolling up, moves up when scrolling down
-      } left-[32px] w-full p-2 h-20 justify-center z-50 text-center pl-4 pr-4 transition-all duration-300 ease-in-out`}
-    >
-      <div
-        className="flex items-center px-5 lg:px-10 py-5 
-      translate-y-10"
-      >
-        <a
-          className="block w-[12rem] xl:mr-8 cursor-pointer"
-          onClick={() => navigate("/")}
-        >
-          <img src={qstate_logo} width={190} height={40} alt="Q State" />
-        </a>
-
-        <nav
-          className={`${
-            openNavigation ? "flex" : "hidden lg:flex"
-          } fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto  lg:bg-opacity-50`}
-        >
-          <div
-            className="flex flex-col items-center w-full 
-           justify-center lg:flex-row cursor-pointer backdrop-blur-lg pl-4 pr-4  bg-opacity-20 border border-n-3 rounded-3xl -translate-x-6"
-          >
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                onClick={() => {
-                  navigate(item.url);
-                }}
-                className={`block font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.url === pathname.pathname
-                    ? "lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } px-6 py-6 lg:py-4 lg:text-sm lg:font-semibold`}
-              >
-                {item.title}
-              </a>
-            ))}
-          </div>
-
-          <HamburgerMenu />
-        </nav>
-        <div className="right-8">
-          <div className="backdrop-blur-xl border border-n-2 flex text-center bg-n-1 bg-opacity-5 translate-y-1 -translate-x-16 rounded-2xl">
-            {!auth.isAuthenticated && (
-              <a
-                onClick={() => navigate("/sign_up")}
-                className="hidden mr-8 text-white translate-y-3 translate-x-4 font-code font-medium transition-colors rounded-xl hover:text-n-3 hover:cursor-pointer lg:block"
-              >
-                REGISTER
-              </a>
-            )}
-            {!auth.isAuthenticated && (
-              <Button
-                className="hidden lg:flex bg-gradient-to-r from-n-3 to-n-4 rounded-2xl border border-l-2 border-l-n-2"
-                onClick={() => navigate("/sign_in")}
-              >
-                Log in
-              </Button>
-            )}
-            {auth.isAuthenticated && (
-              <a
-                onClick={() => {
-                  dispatch(logout());
-                  navigate("/");
-                }}
-                className="hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-              >
-                Logout
-              </a>
-            )}
-            <Button
-              className="ml-auto lg:hidden"
-              px="px-3"
-              onClick={toggleNavigation}
-            >
-              <MenuSvg openNavigation={openNavigation} />
-            </Button>
-          </div>
-        </div>
+    <>
+      <div className="hidden lg:block">
+        <NavBarDesktop />
       </div>
-    </div>
+      <div className="block lg:hidden">
+        <NavBarMobile />
+      </div>
+    </>
   );
 };
-
 export default NavBar;
